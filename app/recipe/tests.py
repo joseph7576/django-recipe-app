97afefb2d7ed89859403dd0ec0aec8892d402ce5
Joseph Hosseini <hosseini7576@gmail.com>
2022-05-28 15:07:49 +0430

    first TDD

--- app/recipe/tests.py
+++ app/recipe/tests.py
@@ -1,8 +1,13 @@
 from django.test import TestCase
-from recipe.calc import add
+from recipe.calc import add, subtract
+
 
 class CalcTests(TestCase):
 
     def test_add_numbers(self):
         """Test that two numbers are added together"""
-        self.assertEqual(add(32,3), 35)
+        self.assertEqual(add(32, 3), 35)
+
+    def test_subtract_numbers(self):
+        """Test that values are subtracted and returned"""
+        self.assertEqual(subtract(5, 4), -1)
