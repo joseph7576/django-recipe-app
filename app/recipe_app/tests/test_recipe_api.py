f3609ff5f1f806f6b0df7d6c0e3bdb2bd3e2ccad
Joseph Hosseini <hosseini7576@gmail.com>
2022-05-31 17:16:56 +0430

    added filtering tags and ingredients

--- app/recipe_app/tests/test_recipe_api.py
+++ app/recipe_app/tests/test_recipe_api.py
@@ -247,7 +247,7 @@ class RecipeImageUploadTests(TestCase):
         tag2 = sample_tag(user=self.user, name='Vegetarian')
         recipe1.tags.add(tag1)
         recipe2.tags.add(tag2)
-        # recipe3 = sample_recipe(user=self.user, title='Fish and chips')
+        recipe3 = sample_recipe(user=self.user, title='Fish and chips')
 
         res = self.client.get(
             RECIPES_URL,
@@ -256,10 +256,10 @@ class RecipeImageUploadTests(TestCase):
 
         serializer1 = RecipeSerializer(recipe1)
         serializer2 = RecipeSerializer(recipe2)
-        # serializer3 = RecipeSerializer(recipe3)
+        serializer3 = RecipeSerializer(recipe3)
         self.assertIn(serializer1.data, res.data)
         self.assertIn(serializer2.data, res.data)
-        # self.assertNotIn(serializer3.data, res.data) TODO: FIX
+        self.assertNotIn(serializer3.data, res.data)
 
     def test_filter_recipes_by_ingredients(self):
         """Test returning recipes with specific ingredients."""
@@ -269,7 +269,7 @@ class RecipeImageUploadTests(TestCase):
         ingredient2 = sample_ingredient(user=self.user, name='Chicken')
         recipe1.ingredients.add(ingredient1)
         recipe2.ingredients.add(ingredient2)
-        # recipe3 = sample_recipe(user=self.user, title='Steak and mushrooms')
+        recipe3 = sample_recipe(user=self.user, title='Steak and mushrooms')
 
         res = self.client.get(
             RECIPES_URL,
@@ -278,7 +278,7 @@ class RecipeImageUploadTests(TestCase):
 
         serializer1 = RecipeSerializer(recipe1)
         serializer2 = RecipeSerializer(recipe2)
-        # serializer3 = RecipeSerializer(recipe3)
+        serializer3 = RecipeSerializer(recipe3)
         self.assertIn(serializer1.data, res.data)
         self.assertIn(serializer2.data, res.data)
-        # self.assertNotIn(serializer3.data, res.data) TODO: FIX
+        self.assertNotIn(serializer3.data, res.data)
